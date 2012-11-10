5e18e8098d388eab10cfa53e20cc87f8591e1721
stokedover9k <stokedover9k@gmail.com>
2012-11-10 12:53:23 -0500

    comparison and hashing methods added

--- src/main/java/vecs/Vecf.java
+++ src/main/java/vecs/Vecf.java
@@ -2,7 +2,7 @@ package vecs;
 
 import java.util.Arrays;
 
-public class Vecf
+public class Vecf implements Comparable<Vecf>
 {
 	double[] _vals;
 	
@@ -24,6 +24,51 @@ public class Vecf
 	
 	public String toString() {
 		return Arrays.toString(_vals);
+	}
+
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(_vals);
+	}
+	
+	@Override
+	public boolean equals( Object o ) {
+		if( o == null ) return false;
+		if( o == this ) return true;
+		return compareTo((Vecf)o) == 0;
+	}
+
+	/* This method does component-wise comparisons, starting at component 0 (X).
+	 * If vectors are not of the same degree, the results are the same as if the
+	 * smaller degree vector was completed with 0's to match the degree of the
+	 * higher degree vector.
+	 * 
+	 *  a = [1, 2, 3]        ---\  [1, 2, 3, 0, 0]  ---\   a < b
+	 *  b = [1, 2, 3, 4, 5]  ---/  [1, 2, 3, 4, 5]  ---/
+	 *  
+	 *  a = [1, 2, 3]        ---\  [1, 2, 3,  0]    ---\   a > b
+	 *  b = [1, 2, 3, -1]    ---/  [1, 2, 3, -1]    ---/
+	 */
+	@Override
+	public int compareTo(Vecf v) {
+		double[] a = _vals, b = v._vals;
+		int flip = 1;
+		if( a.length > b.length ) 
+		{
+			double[] tmp = a;
+			a = b;
+			b = tmp;
+			flip = -1;
+		}   // a now holds the shorter array
+		// for components in both vectors, compare corresponding components
+		for( int i = 0; i < a.length; i++ )
+			if( a[i] < b[i] )        return -flip;
+			else if( a[i] > b[i] )   return  flip;
+		// for components in only the higher degree vector, compare against 0 values
+		for( int i = a.length; i < b.length; i++ )
+			if( b[i] < 0 )       return -flip;
+			else if( b[i] > 0 )  return  flip;
+		return 0;
 	}
 
 }
--- src/main/java/vecs/Veci.java
+++ src/main/java/vecs/Veci.java
@@ -2,7 +2,7 @@ package vecs;
 
 import java.util.Arrays;
 
-public class Veci 
+public class Veci implements Comparable<Veci> 
 {
 	int[] _vals;
 	
@@ -24,5 +24,50 @@ public class Veci
 	
 	public String toString() {
 		return Arrays.toString(_vals);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(_vals);
+	}
+	
+	@Override
+	public boolean equals( Object o ) {
+		if( o == null ) return false;
+		if( o == this ) return true;
+		return compareTo((Veci)o) == 0;
+	}
+
+	/* This method does component-wise comparisons, starting at component 0 (X).
+	 * If vectors are not of the same degree, the results are the same as if the
+	 * smaller degree vector was completed with 0's to match the degree of the
+	 * higher degree vector.
+	 * 
+	 *  a = [1, 2, 3]        ---\  [1, 2, 3, 0, 0]  ---\   a < b
+	 *  b = [1, 2, 3, 4, 5]  ---/  [1, 2, 3, 4, 5]  ---/
+	 *  
+	 *  a = [1, 2, 3]        ---\  [1, 2, 3,  0]    ---\   a > b
+	 *  b = [1, 2, 3, -1]    ---/  [1, 2, 3, -1]    ---/
+	 */
+	@Override
+	public int compareTo(Veci v) {
+		int[] a = _vals, b = v._vals;
+		int flip = 1;
+		if( a.length > b.length ) 
+		{
+			int[] tmp = a;
+			a = b;
+			b = tmp;
+			flip = -1;
+		}   // a now holds the shorter array
+		// for components in both vectors, compare corresponding components
+		for( int i = 0; i < a.length; i++ )
+			if( a[i] != b[i] )
+				return (a[i] - b[i]) * flip;
+		// for components in only the higher degree vector, compare against 0 values
+		for( int i = a.length; i < b.length; i++ )
+			if( b[i] != 0 )
+				return b[i] * flip;
+		return 0;
 	}
 }
